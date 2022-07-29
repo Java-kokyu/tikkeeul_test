9e0f323497fe6d0ff8280323e38bd8dcd0bc3332
Java-kokyu <qwe38280@gmail.com>
2022-07-29 13:32:17 +0900

    프로젝트 복사

---
+++ src/main/java/com/hanghae0705/sbmoney/model/domain/chat/ChatRoomProsCons.java
@@ -0,0 +1,53 @@
+package com.hanghae0705.sbmoney.model.domain.chat;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.hanghae0705.sbmoney.model.domain.chat.entity.ChatRoom;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class ChatRoomProsCons {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Boolean prosCons;
+
+    @Column
+    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "CHATROOM_ID")
+    @JsonBackReference(value = "chatRoom-fk")
+    private ChatRoom chatRoom;
+
+    public ChatRoomProsCons(Boolean prosCons, Long userId, ChatRoom chatRoom){
+        this.prosCons = prosCons;
+        this.userId = userId;
+        this.chatRoom = chatRoom;
+    }
+
+    public Boolean update(Boolean prosCons){
+        this.prosCons = prosCons;
+        return prosCons;
+    }
+
+    public void setChatRoom(ChatRoom chatRoom){
+        this.chatRoom = chatRoom;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        Boolean prosCons;
+    }
+
+}
---
+++ src/main/java/com/hanghae0705/sbmoney/repository/chat/ChatRoomRepository.java
@@ -0,0 +1,17 @@
+package com.hanghae0705.sbmoney.repository.chat;
+
+import com.hanghae0705.sbmoney.model.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    Optional<ChatRoom> findById(Long id);
+    Optional<ChatRoom> findByRoomId(String roomUuid);
+    Optional<ChatRoom> findByRoomIdAndProceeding(String roomUuid, Boolean proceeding);
+    List<ChatRoom> findAllByProceeding(Boolean proceeding);
+    List<ChatRoom> findAllByProceedingOrderByCreatedAtDesc(Boolean proceeding);
+    List<ChatRoom> findAllByUserId(Long userId);
+}
